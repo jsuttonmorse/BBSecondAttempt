111419e387ec552cf570617513a95d40f9abcc16
Jonah <jsuttonmorse@gmail.com>
2012-12-03 14:40:29 -0600

    Created a landing page as well as new game & league management landing pages

---
+++ leaguemanagement.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<title>Manage a League</title>
+</head>
+
+<body>
+	Under Construction.
+	<p>Create New League</p>
+	
+	<p>Existing Leagues</p>
+</body>
+
+</html>
---
+++ newgame.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+	<title>Begin Game</title>
+</head>
+
+<body>
+	Under Construction.
+	<?php echo "Under Construction" ?>
+
+</body>
+
+</html>
