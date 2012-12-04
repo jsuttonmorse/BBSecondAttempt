c151fd38295e5b3b3850df3edfe96a387fdf8d8f
Jonah <jsuttonmorse@gmail.com>
2012-12-04 14:55:10 -0600

    Created the "game" page & linked the landing page there for now.

---
+++ game.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<!--Title-->
+	<title>Game on!</title>
+<!--Styles-->
+
+<!--Scripts-->
+</head>
+
+<body>
+
+	<!--Divs for the basic sections-->
+	<div class="board">
+	Board
+	</div><!--board-->
+	
+	
+	<div class="turns team one">
+	Turns team one
+	</div><!--turns team one-->
+	
+	
+	<div class="turns team two">
+	Turns team two
+	</div><!--turns team two-->
+	
+	
+	<div class="reroll team one">
+	Reroll team one
+	</div><!--reroll team one-->
+	
+	
+	<div class="reroll team two">
+	Reroll team two
+	</div><!--reroll team two-->
+	
+	
+	<div class="score team one">
+	Score team one
+	</div><!--score team one-->
+	
+	
+	<div class="score team two">
+	Score team 2
+	</div><!--score team two-->
+	
+	
+	<div class="dugout team one">
+	Dugout Team 1
+	</div><!--dugout team one--->
+	
+	
+	<div class="dugout team two">
+	Dugout Team 2
+	</div><!--dugout team two-->
+	
+	
+	<div class="knockout team one">
+	KO Team 1
+	</div><!--knockout team one-->
+	
+	
+	<div class="knockout team two">
+	KO Team 2
+	</div><!--knockout team two-->
+	
+	
+	<div class="casualty team one">
+	CAS Team 1
+	</div><!--casualty team one-->
+	
+	
+	<div class="casualty team two">
+	CAS Team 2
+	</div><!--casualty team two-->
+	
+	<div class="dice">
+	Dice
+	</div><!--dice-->
+
+</body>
+
+</html>
