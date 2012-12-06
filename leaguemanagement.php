2cd232f9de096198980949216ac6ac9774df931d
Jonah <jsuttonmorse@gmail.com>
2012-12-05 20:46:30 -0600

    Started league Management
    Added a summary of the current leagues.  Next step is to add a form to
    add a league from here.

--- leaguemanagement.php
+++ leaguemanagement.php
@@ -8,9 +8,42 @@
 
 <body>
 	Under Construction.
-	<p>Create New League</p>
+	<div id="newLeague">
+		<p>Create New League</p>	
+	</div><!--Create new league-->
+	<?php
+		$mysqli = new mysqli("localhost", "root", "root", "BloodBowl02");
+		$result = $mysqli->query("
+				SELECT l.LeagueName, l.leagueOwner, l.MaxTeams
+				, (SELECT count(fkLeagueID) 
+					FROM jtLeagueTeam WHERE 
+					jtLeagueTeam.fkLeagueID = l.LeagueID) AS CurrentTeams
+				FROM league l
+				");
+			if (!$result)
+			{
+				printf("Query failed: %s\n", $mysqli->error);
+  				exit;
+			}
+			while (list($leagueName,
+						$leagueOwner, 
+						$maxTeams,
+						$currentTeams
+						)
+					= $result->fetch_row())
+			{
+			/*So far, I've opened a while loop to show all leagues*/?>
+			
+	<div class="league">
+		<p>League Name: <?php echo $leagueName ?></p>
+		<p>League Owner: <?php echo $leagueOwner ?> </p>
+		<p>Teams: <?php echo $currentTeams ?> of <?php echo $maxTeams ?></p>
+	</div> <!--Individual League Info -->
 	
-	<p>Existing Leagues</p>
+	<?php
+			} /*closing the league while loop*/
+			?>
+		
 </body>
 
 </html>
